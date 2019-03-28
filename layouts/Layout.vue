231734afc86c6fd9ebed89cb45b34600a8d98a7b
Youngsun <lsun@live.cn>
2019-03-28 23:12:45 +0800

    init

---
+++ layouts/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="theme-container">
+        <Content/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Layout"
+    }
+</script>
+
+<style scoped>
+
+</style>
