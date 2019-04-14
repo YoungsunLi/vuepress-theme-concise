65b0faaa940f5da519d531296c610cd91de11772
YoungsunLi <lsun@live.cn>
2019-04-14 17:22:37 +0800

    格式化

--- layouts/404.vue
+++ layouts/404.vue
@@ -1,15 +1,26 @@
 <template>
-    <div>
-        404
+    <div class="theme-container">
+        <div class="content">
+            <h1>🙃404</h1>
+            <blockquote>{{ getMsg() }}</blockquote>
+            <router-link to="/">返回首页.</router-link>
+        </div>
     </div>
 </template>
 
 <script>
+    const msgs = [
+        `There's nothing here.`,
+        `How did we get here?`,
+        `That's a Four-Oh-Four.`,
+        `Looks like we've got some broken links.`
+    ];
+
     export default {
-        name: "404"
+        methods: {
+            getMsg() {
+                return msgs[Math.floor(Math.random() * msgs.length)]
+            }
+        }
     }
 </script>
-
-<style lang="stylus" scoped>
-
-</style>
